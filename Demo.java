3cd7bd0d23c9d865a8e7eadb29cf7d02bf987972
RajputDeepanshu <43953122+RajputDeepanshu@users.noreply.github.com>
2018-10-08 15:26:01 +0530

    Update Demo.java
    First Time uploaded

---
+++ Demo.java
@@ -0,0 +1,17 @@
+class Demo
+{
+String name;
+public Demo(String n)
+[
+name=n;
+}
+public void show()
+{
+System.out.println("My name is : "+name);
+}
+public static void main(String arr[])
+{
+Demo d=new Demo("Deepanshu");
+d.show();
+}
+}
